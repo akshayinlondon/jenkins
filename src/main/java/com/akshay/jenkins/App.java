0228d6ae4f8f6c2e2c32291cac026b833fd160ae
akshayinlondon <akshay.inlondon@gmail.com>
2019-01-06 00:32:27 +0530

    Add files via upload

---
+++ src/main/java/com/akshay/jenkins/App.java
@@ -0,0 +1,17 @@
+package com.akshay.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(sample());
+    }
+    
+    public static String sample() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/akshay/jenkins/AppTest.java
@@ -0,0 +1,15 @@
+package com.akshay.jenkins;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    public void testApp()
+    {
+        assertEquals("Hello World!", App.sample());
+    }
+}
